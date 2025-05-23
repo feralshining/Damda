fef581186cc36d6c239a972f70acc43e1cb33f0d
feralshining <racc00ns@naver.com>
2025-05-23 16:47:43 +0900

    메인 폼 수정

--- Damda/MainForm.Designer.cs
+++ Damda/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Damda
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -30,6 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2Elipse1
@@ -37,14 +40,39 @@
             this.guna2Elipse1.BorderRadius = 20;
             this.guna2Elipse1.TargetControl = this;
             // 
-            // Form1
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(149, 561);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("CookieRun Regular", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "담다";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1014, 561);
+            this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form1";
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "담다 - ";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -52,6 +80,8 @@
         #endregion
 
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Damda/MainForm.cs
+++ Damda/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,77 @@ using System.Windows.Forms;
 
 namespace Damda
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        string sql;
+        public MainForm()
         {
             InitializeComponent();
+            SqlInitialize();
+            InsertSQL();
         }
+        private void SqlInitialize()
+        {
+            string conStr = "Data Source=(LocalDB)\\mylocaldb;AttachDbFilename=C:\\CSharp_SQL\\myDB.mdf;Integrated Security=True;"; //DB경로 연결
+            con = new SqlConnection(conStr);
+            sql = "SELECT * FROM tbMenu";
+            cmd = new SqlCommand(sql, con);
+        }
+
+        private void InsertSQL()
+        {
+            string insertCustomerQuery = @"
+            INSERT INTO tbCustomer (name, phone, registerDate, visitCount, totalSpent, grade, memo) VALUES
+            (N'김다정', '010-1234-5678', '2024-12-10', 3, 150000, N'실버', N'아메리카노 단골'),
+            (N'이지훈', '010-2345-6789', '2024-11-01', 5, 520000, N'골드', N'스탬프 쿠폰 사용 중'),
+            (N'박세영', '010-3456-7890', '2024-10-18', 1, 12000, N'일반', N'처음 방문한 손님'),
+            (N'최유나', '010-1111-2222', '2024-12-20', 2, 78000, N'일반', N'쿠키 자주 구매'),
+            (N'한정민', '010-9999-8888', '2025-01-02', 8, 990000, N'플래티넘', N'VIP, 생일쿠폰 발급 대상');
+            ";
+            string insertSaleQuery = @"
+            INSERT INTO tbSale (customerId, saleDate, item, price, quantity, totalPrice, paymentMethod, memo) VALUES
+            (1, '2025-01-01', N'아메리카노', 5000, 1, 5000, N'카드', N'오전 9시 방문'),
+            (2, '2025-01-02', N'샌드위치 세트', 12000, 2, 24000, N'현금', N'테이크아웃'),
+            (2, '2025-01-04', N'케이크', 28000, 1, 28000, N'카드', N'친구 생일'),
+            (4, '2025-01-03', N'쿠키 3종세트', 9000, 1, 9000, N'계좌이체', NULL),
+            (NULL, '2025-01-03', N'비회원 결제', 8500, 1, 8500, N'현금', N'영수증 요청 안함'),
+            (5, '2025-01-05', N'원두 200g', 25000, 2, 50000, N'카드', N'정기배송 신청자');
+            ";
+
+            if (con.State == ConnectionState.Closed) con.Close();
+            if (con.State != ConnectionState.Open) con.Open();
+
+            cmd = new SqlCommand(insertCustomerQuery, con);
+            cmd.ExecuteNonQuery();
+
+            cmd = new SqlCommand(insertSaleQuery, con);
+            cmd.ExecuteNonQuery();  
+
+
+            //sql = @"INSERT INTO tbMenu (id, name, price)
+            //        VALUES (@id, @name, @price)";
+            //if (con.State != ConnectionState.Open) con.Open();
+            //using (cmd = new SqlCommand(sql, con))
+            //{
+            //    cmd.Parameters.AddWithValue("@id", value1);
+            //    cmd.Parameters.AddWithValue("@name", value2);
+            //    cmd.Parameters.AddWithValue("@price", value3);
+            //}
+            //try
+            //{
+            //    cmd.ExecuteNonQuery();
+            //}
+            //catch (SqlException ex)
+            //{
+            //    if (ex.Number == 2027 || ex.Number == 2601)
+            //        MessageBox.Show(ex.Message, "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+            //MessageBox.Show("정상적으로 등록했습니다", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
     }
 }
